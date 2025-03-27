d7b876b96d7a1ed34666e9309670edb496f0cb40
Matheus Chaves Comin Santana <matheus.santana@itajai.quay.com.br>
2025-03-27 19:41:35 -0300

    Adicionar arquivos de projeto.

---
+++ ROBO.Dominio/AutoMapperConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using EspecificacaoAnalise.Dominio.Entidades;
+using EspecificacaoAnalise.Dominio.Resource.Request;
+using EspecificacaoAnalise.Dominio.Resource.Response;
+
+namespace ROBO.Dominio
+{
+    public class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            var entityAssemplyEntity = typeof(Entidade).Assembly;
+            var entityAssemplyRequest = typeof(EntidadeRequest).Assembly.ExportedTypes.ToList();
+            var entityAssemplyResponse = typeof(EntidadeResponse).Assembly.ExportedTypes.ToList();
+
+            entityAssemplyEntity.ExportedTypes.ToList().ForEach(s =>
+            {
+                var formattedRequestModelName = string.Format("{0}Request", s.Name);
+                var requestModelName = entityAssemplyRequest.FirstOrDefault(s => s.Name == formattedRequestModelName);
+                if (requestModelName != null)
+                {
+                    this.CreateMap(s, requestModelName).ReverseMap();
+                }
+
+                var formattedResponseName = string.Format("{0}Response", s.Name);
+                var responseModelName = entityAssemplyResponse.FirstOrDefault(s => s.Name == formattedResponseName);
+                if (responseModelName != null)
+                {
+                    var map = this.CreateMap(s, responseModelName).ReverseMap();
+
+                    CreateMap<Entidade, EntidadeResponse>()
+                    .ForMember(dest => dest.ProjetoDescricao, opt =>
+                    {
+                        opt.PreCondition(src => src.Projeto != null);
+                        opt.MapFrom(src => src.Projeto.Descricao);
+                    });
+
+                    CreateMap<EntidadeAcao, EntidadeAcaoResponse>()
+                    .ForMember(dest => dest.EntidadeDescricao, opt =>
+                    {
+                        opt.PreCondition(src => src.Entidade != null);
+                        opt.MapFrom(src => src.Entidade.Descricao);
+                    })
+                    .ForMember(dest => dest.AcaoDescricao, opt =>
+                    {
+                        opt.PreCondition(src => src.Acao != null);
+                        opt.MapFrom(src => src.Acao.Descricao);
+                    });
+                }
+            });
+
+        }
+    }
+}
---
+++ ROBO.Dominio/Entidades/Base/EntidadeBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ROBO.Dominio.Entidades.Base
+{
+    public class EntidadeBase
+    {
+        public long Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
